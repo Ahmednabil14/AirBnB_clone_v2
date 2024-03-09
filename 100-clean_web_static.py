0c012e48cd05a8542168a3b94903e41fbd6f86d6
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-10 01:39:13 +0200

    task 100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,7 +9,7 @@ def do_clean(number=0):
     versions = os.listdir("versions/")
     versions_date_time = []
     for ver in versions:
-        versions_date_time.append(ver.split("_")[2][:-4])
+        versions_date_time.append(int(ver.split("_")[2][:-4]))
     versions_date_time.sort()
     print(versions_date_time)
 do_clean()

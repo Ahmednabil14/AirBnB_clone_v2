e9c44ae329ed08a971bbb6269eee687bf2fcba61
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-10 01:31:54 +0200

    task 100

---
+++ 100-clean_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""clean"""
+from fabric.api import local, env, run
+import os
+
+
+def do_clean(number=0):
+    """fabric function"""
+    versions = os.listdir("versions/")
+    versions_date_time = []
+    for ver in versions:
+        versions_date_time.append(ver.split("_")[2][:-4])      
+    print(versions_date_time)

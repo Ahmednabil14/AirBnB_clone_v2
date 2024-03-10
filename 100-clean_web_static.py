a9c3a2f5a1a39610ae6077d44f2289bb1c030798
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-10 16:55:49 +0200

    task 100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """clean"""
 from fabric.api import local, env, run
-from fabric.decorators import runs_once
 import os
 
 
 env.hosts = ['54.165.77.224', '100.24.242.177']
 
 
-@runs_once
 def do_clean(number=0):
     """fabric function"""
     versions = os.listdir("versions/")
@@ -16,9 +14,9 @@ def do_clean(number=0):
     for ver in versions:
         versions_date_time.append(int(ver.split("_")[2][:-4]))
     versions_date_time.sort()
+    print(versions)
     if number == "0" or number == "1":
         for ver in versions:
             if str(versions_date_time[-1]) not in ver and str(versions_date_time[-2]) not in ver:
                 local(f"rm -rf versions/{ver}")
                 run(f"rm -rf /data/web_static/releases/versions/{ver[:-4]}")
-                print(versions)

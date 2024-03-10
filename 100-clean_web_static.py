0951fb198668c4209d544c20f7ee735c53d173ec
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-10 15:55:01 +0200

    task 100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -15,9 +15,8 @@ def do_clean(number=0):
         versions_date_time.append(int(ver.split("_")[2][:-4]))
     versions_date_time.sort()
     if number == "0" or number == "1":
-        print("ok")
         for ver in versions:
-            if str(versions_date_time[-1]) not in ver and str(versions_date_time[-2]) not in ver:
+            if str(versions_date_time[-1]) not in ver or str(versions_date_time[-2]) not in ver:
                 print(f"Considering for deletion: {ver}")
                 local(f"rm -rf versions/{ver}")
                 run(f"rm -rf /data/web_static/releases/versions/{ver[:-4]}")

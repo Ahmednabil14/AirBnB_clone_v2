614c595ee0b472b35e7f1ddbcf0e3f5999487ce5
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-10 14:29:52 +0200

    task 100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,20 +9,15 @@ env.hosts = ['100.24.242.177', '54.165.77.224']
 
 def do_clean(number=0):
     """fabric function"""
-    # print("do_clean function called")
     versions = os.listdir("versions/")
     versions_date_time = []
     for ver in versions:
         versions_date_time.append(int(ver.split("_")[2][:-4]))
     versions_date_time.sort()
-    print("number = {}".format(number))
     if number == "0" or number == "1":
         print("ok")
         for ver in versions:
-            if versions_date_time[-1] not in ver and versions_date_time[-2] not in ver:
+            if str(versions_date_time[-1]) not in ver and versions_date_time[-2] not in ver:
                 print(f"Considering for deletion: {ver}")
                 local(f"rm -rf /versions/{ver}")
                 run(f"rm -rf /data/web_static/releases/versions/{ver}")
-    else:
-        print("else statement")
-        print(type(number))
